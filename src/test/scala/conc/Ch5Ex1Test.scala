96e13825f187c2c7c59f197b010e77d54345e9b2
serbaev.ilnur <serbaevilnur@gmail.com>
2019-01-08 17:41:07 +0500

    Chapter5 ex1

--- src/test/scala/conc/Ch5Ex1Test.scala
+++ src/test/scala/conc/Ch5Ex1Test.scala
@@ -12,4 +12,49 @@ object Ch5Ex1Test extends App {
   val avg = all.seq.sum / index
   println()
   println(s"avg = $avg")
+
+
+  object Timed {
+
+    @volatile
+    var dummy: Any = _
+
+    def buildObjects(count:Int) = {
+      var i = 0
+      val start = System.nanoTime
+      while (i < count) {
+        dummy = new Object
+        i += 1
+      }
+      (System.nanoTime - start)/count.toDouble
+    }
+
+  }
+
+  var i = 0
+  var summ = 0D
+
+  var timePrev = 0D
+  while (i < 30) {
+
+    val time = Timed.buildObjects(10000000)
+    val e = Math.abs(time - timePrev)/time*100
+
+    //check steady state
+    if (e < 10) {
+      i += 1
+      summ += time
+    } else {
+      i = 0
+      summ = time
+    }
+
+    timePrev = time
+    log(s"time = ${time.toString} e = ${Math.round(e)}, i = $i")
+
+  }
+
+  log("----------------------------------------------------")
+  log(s"avg = ${summ/(i+1)} nanoseconds")
+
 }
